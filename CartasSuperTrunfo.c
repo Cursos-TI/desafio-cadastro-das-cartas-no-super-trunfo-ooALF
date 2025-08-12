7bdda65213d20acf326f0d22f5b64ab969148a99
João Victor  Costa <jbezerra30@hotmail.com>
2025-08-12 13:08:05 +0000

    calculos

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,16 +5,17 @@ int main (){
 
 
 char cidade[20], cidade2[20];
-char estado[10], estado2[10];
+char estado[20], estado2[20];
 char codigo[20],codigo2[20];
-int populacao, populacao2;  
+float populacao, populacao2;  
 int turistico, turistico2;
 float area,area2;
 float pib, pib2;
+float divisao, divisao2;
+float areadiv, areadiv2;
 
 
 //carta 1 do jogo
-
 printf ("Primeira letra do Estado:\n");
 scanf ("%s",&estado);
 printf("Codigo da carta de 1 à 4 (ex A01, A03):\n");
@@ -30,7 +31,9 @@ scanf ("%f",&area);
 printf ("Pib:\n");
 scanf ("%f",&pib);
 
-//carta 1 do jogo
+
+
+//carta 2 do jogo
 
 printf ("Primeira letra do Estado:\n");
 scanf ("%s",&estado2);
@@ -46,25 +49,38 @@ printf ("Área em km²:\n");
 scanf ("%f",&area2);
 printf ("Pib:\n");
 scanf ("%f",&pib2);
+printf ("\n");
 
+divisao = (pib + area) / 2;
+divisao2 = (pib2 + area2) / 2;
+
+
+//exibição carta 1
 printf ("Carta 1:\n");
-printf ("Estado:%s\n" ,estado);
-printf ("Codigo:%s\n" ,codigo);
-printf ("Cidade:%s\n" ,cidade);
-printf ("População:%f\n",populacao);
-printf ("Area:%f\n",area);
-printf ("Pib:%f\n",pib);
-printf ("Pontos Turisticos:%d\n",turistico);
+printf ("Estado: %s\n" ,estado);
+printf ("Codigo: %s\n" ,codigo);
+printf ("Cidade: %s\n" ,cidade);
+printf ("População: %f Habitantes\n",populacao);
+printf ("Area: %.2f km²\n",area);
+printf ("Pib: %.2f\n bilhões\n",pib);
+printf ("Pontos Turisticos: %d\n",turistico);
+printf ("PIB per Capita %.3f reais\n",divisao);
+printf ("Densidade populacional %.2f hab/km²\n",areadiv);
+printf ("\n");
+areadiv = ( populacao + area) / 2;
+areadiv2 = ( populacao2 + area2) / 2;
 
-
+//exibição carta 2
 printf ("Carta 2:\n");
 printf ("Estado:%s\n" ,estado2);
 printf ("Codigo:%s\n" ,codigo2);
 printf ("Cidade:%s\n" ,cidade2);
-printf ("População:%f\n",populacao2);
-printf ("Area:%f\n",area2);
-printf ("Pib:%f\n",pib2);
+printf ("População:%f Habitantes\n",populacao2);
+printf ("Area:%.2f km²\n",area2);
+printf ("Pib:%.2f Bilhões\n",pib2);
 printf ("Pontos Turisticos:%d\n",turistico2);
+printf ("PIB per Capita %.2f reais \n",divisao2);
+printf ("Densidade populacional %.2f hab/km²\n",areadiv2);
 
-    return 0;
+return 0;
 }
