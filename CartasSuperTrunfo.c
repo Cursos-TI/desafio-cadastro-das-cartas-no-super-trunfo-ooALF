93ff57837b7d94be711008e720eb8ff4ebaad581
João Victor  Costa <jbezerra30@hotmail.com>
2025-08-14 18:48:34 +0000

    finalizado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,13 +7,14 @@ int main (){
 char cidade[20], cidade2[20];
 char estado[20], estado2[20];
 char codigo[20],codigo2[20];
-float populacao, populacao2;  
+unsigned long int populacao, populacao2;  
 int turistico, turistico2;
 float area,area2;
 float pib, pib2;
 float divisao, divisao2;
 float areadiv, areadiv2;
-
+float deninverso, deninverso2;
+float superpoder, superpoder2;
 
 //carta 1 do jogo
 printf ("Primeira letra do Estado:\n");
@@ -23,7 +24,7 @@ scanf("%s",&codigo);
 printf ("Nome da Cidade:\n");
 scanf ("%s",&cidade);
 printf ("População da Cidade:\n");
-scanf ("%f",&populacao);
+scanf ("%lu",&populacao);
 printf ("Pontos Turisticos:\n");
 scanf ("%d",&turistico);
 printf ("Área em km²:\n");
@@ -42,7 +43,7 @@ scanf("%s",&codigo2);
 printf ("Nome da Cidade:\n");
 scanf ("%s",&cidade2);
 printf ("População da Cidade:\n");
-scanf ("%f",&populacao2);
+scanf ("%lu",&populacao2);
 printf ("Pontos Turisticos:\n");
 scanf ("%d",&turistico2);
 printf ("Área em km²:\n");
@@ -54,33 +55,90 @@ printf ("\n");
 divisao = (pib + area) / 2;
 divisao2 = (pib2 + area2) / 2;
 
+areadiv = ( populacao + area) / 2;
+areadiv2 = ( populacao2 + area2) / 2;
+
+deninverso = 1/areadiv;
+deninverso2 = 1/areadiv2;
+
+superpoder = populacao + area + pib + turistico + divisao + deninverso;
+superpoder2 = populacao2 + area2 + pib2 + turistico2 + divisao2 + deninverso2;
 
 //exibição carta 1
 printf ("Carta 1:\n");
 printf ("Estado: %s\n" ,estado);
 printf ("Codigo: %s\n" ,codigo);
 printf ("Cidade: %s\n" ,cidade);
-printf ("População: %f Habitantes\n",populacao);
+printf ("População: %.4lu Habitantes\n",populacao);
 printf ("Area: %.2f km²\n",area);
 printf ("Pib: %.2f\n bilhões\n",pib);
 printf ("Pontos Turisticos: %d\n",turistico);
 printf ("PIB per Capita %.3f reais\n",divisao);
 printf ("Densidade populacional %.2f hab/km²\n",areadiv);
+printf ("Super Poder: %.2f\n",superpoder);
 printf ("\n");
-areadiv = ( populacao + area) / 2;
-areadiv2 = ( populacao2 + area2) / 2;
+
 
 //exibição carta 2
 printf ("Carta 2:\n");
 printf ("Estado:%s\n" ,estado2);
 printf ("Codigo:%s\n" ,codigo2);
 printf ("Cidade:%s\n" ,cidade2);
-printf ("População:%f Habitantes\n",populacao2);
+printf ("População:%.4lu Habitantes\n",populacao2);
 printf ("Area:%.2f km²\n",area2);
 printf ("Pib:%.2f Bilhões\n",pib2);
 printf ("Pontos Turisticos:%d\n",turistico2);
 printf ("PIB per Capita %.2f reais \n",divisao2);
 printf ("Densidade populacional %.2f hab/km²\n",areadiv2);
+printf ("Super Poder: %.2f\n",superpoder2);
+
+
+
+printf("\n");
+//VENDEDOR 
+
+if (area > area2){
+    printf("1\n");
+} else {
+    printf("0\n");
+}
+
+
+
+if ( pib > pib2){
+    printf("1\n");
+} else {
+    printf("0\n");
+}
+
+
+if (turistico > turistico2){
+    printf("1\n");
+} else {
+    printf("0\n");
+}
+
+
+if (divisao > divisao2){
+    printf("1\n");
+} else {
+    printf("0\n");
+}
+
+
+
+if (areadiv > areadiv2){
+    printf("1\n");
+} else {
+    printf("0\n");
+}
+
+
+if (superpoder > superpoder2){
+    printf("1\n");
+} else {
+    printf("0\n");
+}
 
 return 0;
 }
